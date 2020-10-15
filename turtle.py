d390cfd8f566166163b9075c7f9632c33a033be5
Josh Wolsborn <josh.wolsborn@seeq.com>
2020-10-14 17:27:16 -0700

    refactor

--- turtle.py
+++ turtle.py
@@ -7,36 +7,7 @@ for side in range(3): #see number three? its what reminds Python,  THREE times!
   pen.right(90)
 pen.forward(100)
 
-player = turtle.Turtle()
-player.penup()
-
-def moveRight():
-    x = player.xcor()
-    x += 10
-    player.setx(x)
-
-def moveLeft():
-    x = player.xcor()
-    x -= 10
-    player.setx(x)  
-
-def moveForward():
-    print('something')
-    y = player.ycor()
-    y += 10
-    player.sety(y)
-
-def moveBackward():
-    y = player.ycor()
-    y -= 10
-    player.sety(y)
-
 wn = turtle.Screen()
 wn.listen()
-
-wn.onkey(moveRight, 'd')
-wn.onkey(moveLeft, 'a')
-wn.onkey(moveForward, 'w')
-wn.onkey(moveBackward, 's')
 
 turtle.done() #this just keeps the window open until we close it
