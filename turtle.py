bde0bf781a32026bd174fc3f26c881aca8291428
Josh Wolsborn <josh.wolsborn@seeq.com>
2020-10-14 17:18:12 -0700

    First commit

---
+++ turtle.py
@@ -0,0 +1,42 @@
+import turtle
+pen = turtle.Turtle()
+
+pen.pendown() #this line puts the pen on the paper
+for side in range(3): #see number three? its what reminds Python,  THREE times!
+  pen.forward(100)
+  pen.right(90)
+pen.forward(100)
+
+player = turtle.Turtle()
+player.penup()
+
+def moveRight():
+    x = player.xcor()
+    x += 10
+    player.setx(x)
+
+def moveLeft():
+    x = player.xcor()
+    x -= 10
+    player.setx(x)  
+
+def moveForward():
+    print('something')
+    y = player.ycor()
+    y += 10
+    player.sety(y)
+
+def moveBackward():
+    y = player.ycor()
+    y -= 10
+    player.sety(y)
+
+wn = turtle.Screen()
+wn.listen()
+
+wn.onkey(moveRight, 'd')
+wn.onkey(moveLeft, 'a')
+wn.onkey(moveForward, 'w')
+wn.onkey(moveBackward, 's')
+
+turtle.done() #this just keeps the window open until we close it
